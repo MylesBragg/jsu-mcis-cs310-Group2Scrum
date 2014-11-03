086a0e9bc9f96ecd81bfa5c501b053878a00ca6b
Darryl Cummins <dcummins@jsu.edu>
2014-11-03 13:29:30 -0600

    fix acceptance test reference

--- runacceptance.bat
+++ runacceptance.bat
@@ -1,4 +1,4 @@
 cd acceptance
 javac -cp .;..\build\classes\main ArgumentParserKeywords.java
-java -cp .;..\build\classes\main;C:\RobotFramework\robotframework-2.8.5.jar;C:\RobotFramework\swinglibrary-1.5.1.jar org.robotframework.RobotFramework ArgumentParserTests.txt
+java -cp .;..\build\classes\main;C:\RobotFramework\robotframework-2.8.5.jar;C:\RobotFramework\swinglibrary-1.5.1.jar org.robotframework.RobotFramework ArgParserTest.txt
 cd ..
