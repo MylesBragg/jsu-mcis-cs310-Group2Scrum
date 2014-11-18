48ec8abdc24578740cfeac76a6aaf3bcd123cf74
eparham7861 <eparham7861@hotmail.com>
2014-11-18 12:04:28 -0600

    add XML style main

--- src/main/WriteAndSaveParser.java
+++ src/main/WriteAndSaveParser.java
@@ -2,15 +2,15 @@ import edu.jsu.mcis.*;
 import java.io.*;
 public class WriteAndSaveParser {
 	public static void main(String[] args) {
-		File f = new File("Volume Calcluator.xml");
+		File f = new File("Volume Calculator.xml");
 		ArgumentParser parser;
 		
 		if(f.exists()) {
-			System.out.println("I am reading from an xml file.");
+			System.out.println("\nI am reading from an xml file.");
 			parser = XMLFactory.createArgumentParser("Volume Calculator", f.getName());
 		}
 		else {
-			System.out.println("I am making an xml file.");
+			System.out.println("\nI am making an xml file.");
 			parser = new ArgumentParser("Volume Calculator");
 			parser.addPositionalArgument("length", Argument.Type.FLOAT);
 			parser.addPositionalArgument("width", Argument.Type.FLOAT);
@@ -24,7 +24,7 @@ public class WriteAndSaveParser {
 		length = parser.getArgumentValue("length");
 		width = parser.getArgumentValue("width");
 		height = parser.getArgumentValue("height");
-		System.out.println("length: " + length);
+		System.out.println("\nlength: " + length);
 		System.out.println("width: " + width);
 		System.out.println("height: " + height);
 		System.out.println("Volume: " + (length * width * height));
