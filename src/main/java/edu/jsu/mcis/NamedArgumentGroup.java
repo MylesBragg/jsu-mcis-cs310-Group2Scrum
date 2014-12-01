aa23de2f1af218487d78e1649972f83a4f5144b8
eparham7861 <eparham7861@hotmail.com>
2014-12-01 09:06:52 -0600

    add named argument group class

---
+++ src/main/java/edu/jsu/mcis/NamedArgumentGroup.java
@@ -0,0 +1,47 @@
+package edu.jsu.mcis;
+
+import java.util.*;
+public class NamedArgumentGroup 
+{
+	private String groupHeader;
+	private List<String> group;
+	
+	public NamedArgumentGroup(String name)
+	{
+		groupHeader = name;
+		group = new ArrayList<String>();
+	}
+	
+	public void appendGroupMember(String name)
+	{
+		group.add(name);
+	}
+	
+	public void removeGroupMember(String name)
+	{
+		group.remove(name);
+	}
+	
+	public boolean checkGroupHeader(String name)
+	{
+		if (name.equals(groupHeader))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public boolean checkGroup(String name)
+	{
+		if (group.contains(name))
+		{
+			return true;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
